261e3bf8944ddd6332d29f15f114cd2f553fe87f
doit <w3cfuns@qq.com>
2021-02-18 15:57:53 +0800

    feat: 矩阵顺时针，思路：人转一圈即可

---
+++ leetcode/剑指offer-29.js
@@ -0,0 +1,51 @@
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+  if (matrix.length === 0) return []
+  const m = matrix.length, n = matrix[0].length
+  const result = []
+  let startX = 0
+  , endX = n - 1
+  , startY = 0
+  , endY = m - 1
+  , direct = 'left'
+  const walk = {
+      left () {
+          for(let i = startX; i <= endX; i++) {
+              result.push(matrix[startY][i])
+          }
+          startY++
+          direct = 'down'
+      },
+      down () {
+          for(let i = startY; i <= endY; i++) {
+              result.push(matrix[i][endX])
+          }
+          endX--
+          direct = 'right'
+      },
+      right () {
+          for(let i = endX; i >= startX; i--) {
+              result.push(matrix[endY][i])
+          }
+          endY--
+          direct = 'up'
+      },
+      up () {
+          for(let i = endY; i >= startY; i--) {
+              result.push(matrix[i][startX])
+          }
+          startX++
+          direct = 'left'
+      }
+  }
+  while(result.length < m * n) {
+      console.log(direct, result)
+      walk[direct]()
+  }
+  return result
+};
+// @lc code=end
